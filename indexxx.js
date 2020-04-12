d673ece570946d74c4937f172156d9fb89568dbc
Filipasek <filipasek10@gmail.com>
2020-04-12 21:43:38 +0200

    serving thread list (avatars only in groups)

--- indexxx.js
+++ indexxx.js
@@ -12,15 +12,7 @@ const app = express();
 app.use(express.static("public"));
 
 function getDateFromTimestamp(timestamp) {
-    var date = new Date(timestamp * 1);
-    // Hours part from the timestamp
-    var hours = date.getHours();
-    // Minutes part from the timestamp
-    var minutes = "0" + date.getMinutes();
-
-    // Will display time in 10:30:23 format
-    var formattedTime = hours + ':' + minutes.substr(-2);
-
+    var formattedTime = new Date(timestamp / 1).toLocaleString("pl-PL")
     return formattedTime;
 }
 
@@ -44,22 +36,21 @@ app.get("/threadList", (request, response) => {
     var pre_credentials = request.get('JSON-Credentials');
     // response.json(credentials)
     if (pre_credentials === undefined) {
-        console.error("Nie można odczytać danych!")
         response.send("Nie można odczytać danych!")
     } else {
         var credentials = JSON.parse(pre_credentials)
             // var credentials = [{ "key": "datr", "value": "5IiQXkEbkAne00pKn_CBj1DV", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2020-04-10T14:55:33.099Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "fr", "value": "1onJo8ezsPzlPOBJ4.AWXll299FwYpBacHAs9A0LnVjzo.BekIjk.8X.AAA.0.0.BekIjl.AWXwcq5W", "expires": "2020-07-09T14:55:29.000Z", "maxAge": 7775996, "domain": "facebook.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:33.128Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "sb", "value": "5IiQXir6ev4oAWWxZTnji4kp", "expires": "2022-04-10T14:55:33.000Z", "maxAge": 63072000, "domain": "facebook.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:33.131Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "c_user", "value": "100049996898966", "expires": "2021-04-10T14:55:31.000Z", "maxAge": 31535998, "domain": "facebook.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.716Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "xs", "value": "32%3A3feyOm4uDTvDWg%3A2%3A1586530533%3A-1%3A-1", "expires": "2021-04-10T14:55:31.000Z", "maxAge": 31535998, "domain": "facebook.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.718Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "spin", "value": "r.1001973156_b.trunk_t.1586530535_s.1_v.2_", "expires": "2020-04-11T15:55:35.000Z", "maxAge": 90000, "domain": "facebook.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.888Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "presence", "value": "EDvF3EtimeF1586530538EuserFA21B49996898966A2EstateFDutF0Et2F_5b_5dElm2FnullEuct2F1586530538966EtrFnullEtwF1874024300EatF1586530538966CEchFDp_5f1B49996898966F0CC", "domain": "facebook.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:38.968Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "locale", "value": "en_US", "domain": "facebook.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:38.972Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "a11y", "value": "%7B%22sr%22%3A0%2C%22sr-ts%22%3A1586530538976%2C%22jk%22%3A0%2C%22jk-ts%22%3A1586530538976%2C%22kb%22%3A0%2C%22kb-ts%22%3A1586530538976%2C%22hcm%22%3A0%2C%22hcm-ts%22%3A1586530538976%7D", "domain": "facebook.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:38.977Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "datr", "value": "5IiQXkEbkAne00pKn_CBj1DV", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2020-04-10T14:55:33.099Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "fr", "value": "1onJo8ezsPzlPOBJ4.AWXll299FwYpBacHAs9A0LnVjzo.BekIjk.8X.AAA.0.0.BekIjl.AWXwcq5W", "expires": "2020-07-09T14:55:29.000Z", "maxAge": 7775996, "domain": "facebook.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:33.128Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "sb", "value": "5IiQXir6ev4oAWWxZTnji4kp", "expires": "2022-04-10T14:55:33.000Z", "maxAge": 63072000, "domain": "facebook.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:33.131Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "c_user", "value": "100049996898966", "expires": "2021-04-10T14:55:31.000Z", "maxAge": 31535998, "domain": "facebook.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.716Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "xs", "value": "32%3A3feyOm4uDTvDWg%3A2%3A1586530533%3A-1%3A-1", "expires": "2021-04-10T14:55:31.000Z", "maxAge": 31535998, "domain": "facebook.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.718Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "spin", "value": "r.1001973156_b.trunk_t.1586530535_s.1_v.2_", "expires": "2020-04-11T15:55:35.000Z", "maxAge": 90000, "domain": "facebook.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.888Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "presence", "value": "EDvF3EtimeF1586530538EuserFA21B49996898966A2EstateFDutF0Et2F_5b_5dElm2FnullEuct2F1586530538966EtrFnullEtwF1874024300EatF1586530538966CEchFDp_5f1B49996898966F0CC", "domain": "facebook.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:38.968Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "locale", "value": "en_US", "domain": "facebook.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:38.972Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "a11y", "value": "%7B%22sr%22%3A0%2C%22sr-ts%22%3A1586530538976%2C%22jk%22%3A0%2C%22jk-ts%22%3A1586530538976%2C%22kb%22%3A0%2C%22kb-ts%22%3A1586530538976%2C%22hcm%22%3A0%2C%22hcm-ts%22%3A1586530538976%7D", "domain": "facebook.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:38.977Z", "lastAccessed": "2020-04-10T14:55:38.985Z" }, { "key": "sb", "value": "5IiQXir6ev4oAWWxZTnji4kp", "expires": "2022-04-10T14:55:33.000Z", "maxAge": 63072000, "domain": "messenger.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.726Z", "lastAccessed": "2020-04-10T14:55:38.988Z" }, { "key": "c_user", "value": "100049996898966", "expires": "2021-04-10T14:55:31.000Z", "maxAge": 31535998, "domain": "messenger.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.730Z", "lastAccessed": "2020-04-10T14:55:38.988Z" }, { "key": "xs", "value": "32%3A3feyOm4uDTvDWg%3A2%3A1586530533%3A-1%3A-1", "expires": "2021-04-10T14:55:31.000Z", "maxAge": 31535998, "domain": "messenger.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.734Z", "lastAccessed": "2020-04-10T14:55:38.988Z" }, { "key": "fr", "value": "1onJo8ezsPzlPOBJ4.AWXll299FwYpBacHAs9A0LnVjzo.BekIjk.8X.AAA.0.0.BekIjl.AWXwcq5W", "expires": "2020-07-09T14:55:29.000Z", "maxAge": 7775996, "domain": "messenger.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:35.737Z", "lastAccessed": "2020-04-10T14:55:38.988Z" }, { "key": "spin", "value": "r.1001973156_b.trunk_t.1586530535_s.1_v.2_", "expires": "2020-04-11T15:55:35.000Z", "maxAge": 90000, "domain": "messenger.com", "path": "/", "secure": true, "httpOnly": true, "hostOnly": false, "creation": "2020-04-10T14:55:37.421Z", "lastAccessed": "2020-04-10T14:55:38.988Z" }, { "key": "presence", "value": "EDvF3EtimeF1586530538EuserFA21B49996898966A2EstateFDutF0Et2F_5b_5dElm2FnullEuct2F1586530538966EtrFnullEtwF1874024300EatF1586530538966CEchFDp_5f1B49996898966F0CC", "domain": "messenger.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:38.970Z", "lastAccessed": "2020-04-10T14:55:38.988Z" }, { "key": "locale", "value": "en_US", "domain": "messenger.com", "path": "/", "secure": true, "hostOnly": false, "creation": "2020-04-10T14:55:38.974Z", "lastAccessed": "2020-04-10T14:55:38.988Z" }]
         login({ appState: JSON.parse(JSON.stringify(credentials)) }, (err, api) => {
             if (err) return console.error(err);
-            api.getThreadList(10, null, [], (err, list) => {
+            api.getThreadList(20, null, [], (err, list) => {
                 if (err) return console.error(err)
-                var history_json = [{}];
-                var k = 0;
-                list.reduce((promiseChain, item) => {
-                    return promiseChain.then(() => new Promise((resolve) => {
+                var history_json = {};
+                var k = "";
+                let requests = list.map((item) => {
+                    return new Promise((resolve) => {
                         // console.log(item.name)
                         // console.log(getDateFromTimestamp(item.timestamp))
-                        // k = item.threadID;
+                        k = item.threadID;
                         api.getThreadHistory(item.threadID, 1, undefined, (err, history) => {
                             if (err) return console.error(err)
                             var isRead;
@@ -78,29 +69,22 @@ app.get("/threadList", (request, response) => {
                                 // })
                                 whose = "Nie Ty";
                             }
-                            var message;
-                            if (history[0].type === "event") {
-                                message = history[0].snippet;
-                            } else {
-                                message = history[0].body;
-                            }
                             history_json[k] = {
                                 name: item.name,
                                 threadID: item.threadID,
-                                last_message: message,
+                                last_message: history[0].body,
                                 isRead: isRead,
                                 imageSrc: item.imageSrc,
                                 who: whose,
                                 when: getDateFromTimestamp(item.timestamp),
-                                service: "Messenger",
                             }
-                            k++;
 
                             resolve()
                         })
 
-                    }));
-                }, Promise.resolve()).then(() => response.json(JSON.parse(JSON.stringify(history_json)))).catch((err) => console.error(err))
+                    });
+                });
+                Promise.all(requests).then(() => response.json(JSON.parse(JSON.stringify(history_json)))).catch((err) => console.error(err))
             })
         })
     }
